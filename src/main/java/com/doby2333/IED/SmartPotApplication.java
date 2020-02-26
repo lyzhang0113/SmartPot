d89d1a71676c8eb902f94835b99243a3379e5070
Doby2333 <zly2000@hotmail.com>
2020-02-25 20:29:58 -0500

    Initial commit

---
+++ src/main/java/com/doby2333/IED/SmartPotApplication.java
@@ -0,0 +1,12 @@
+package com.doby2333.IED;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartPotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartPotApplication.class, args);
+    }
+}
